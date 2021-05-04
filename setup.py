6ea072689475195faa0ac862e0559cf5f05b0190
Tom McClintock <tmcclintock89@gmail.com>
2021-05-04 08:35:11 -0700

    black on setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 
-dist = setup(name="monopolymath",
-             author="Thomas McClintock",
-             author_email="mcclintock@bnl.gov",
-             description="Tool for simulating Monopoly.",
-             license="MIT",
-             url="https://github.com/tmcclintock/MonopolyMath",
-             python_requires='>=3.5',
-             packages=['monopolymath'],
-             package_dir={'monopolymath': 'monopolymath'},
-             package_data={'monopolymath': ['*.txt']},
-             include_package_data=True,
-             long_description=open("README.md").read()
+dist = setup(
+    name="monopolymath",
+    author="Thomas McClintock",
+    author_email="mcclintock@bnl.gov",
+    description="Tool for simulating Monopoly.",
+    license="MIT",
+    url="https://github.com/tmcclintock/MonopolyMath",
+    python_requires=">=3.5",
+    packages=["monopolymath"],
+    package_dir={"monopolymath": "monopolymath"},
+    package_data={"monopolymath": ["*.txt"]},
+    include_package_data=True,
+    long_description=open("README.md").read(),
 )
